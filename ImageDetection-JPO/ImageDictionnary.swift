cede3b0e494019658e42924dee5055f857a3b0c7
Robin Champsaur <robin.champsaur@3ie.fr>
2018-06-28 14:40:58 +0200

    [Gitignore] Added gitignore

---
+++ ImageDetection-JPO/ImageDictionnary.swift
@@ -0,0 +1,9 @@
+//
+//  ImageDictionnary.swift
+//  ImageDetection-JPO
+//
+//  Created by Robin Champsaur on 28/06/2018.
+//  Copyright © 2018 Robin Champsaur. All rights reserved.
+//
+
+import Foundation
