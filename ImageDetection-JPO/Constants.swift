8e944b77fadd7871aab89bf8ae81487f90548d8e
Robin Champsaur <robin.champsaur@3ie.fr>
2018-07-03 15:47:34 +0200

    [Constants] Added Constants file

---
+++ ImageDetection-JPO/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  ImageDetection-JPO
+//
+//  Created by Robin Champsaur on 03/07/2018.
+//  Copyright © 2018 Robin Champsaur. All rights reserved.
+//
+
+import Foundation
+
+public class Constants {
+
+    struct ARReference {
+
+        static var folderName = "WorldCup"
+
+    }
+
+}
